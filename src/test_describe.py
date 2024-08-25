b2a188045abb4aa6dfab822f0e213bc24faf460d
haranku16 <haranku16@users.noreply.github.com>
2024-08-25 11:02:43 -0400

    Implementing describe script and unit test

--- src/test_describe.py
+++ src/test_describe.py
@@ -1,2 +1,5 @@
-def test_main():
-    assert 1 == 1
+from describe import describe
+
+def test_describe():
+    with open('test_data/films_described.txt', 'r') as expected:
+        assert describe('test_data/films.csv').to_string() == expected.read()
